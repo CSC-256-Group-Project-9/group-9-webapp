11c18753841c0c978592b00de91764b6de7952a4
Anthony E <AnthonyDEpps@gmail.com>
2023-10-19 14:58:15 -0400

    feature: test5: list has been added - ugly but works for testing purposes

--- static/js/testing.js
+++ static/js/testing.js
@@ -1,16 +1,19 @@
 // test variables
 let test1_value = ''
 let test2_value = 0
+let test5_list = []
 
 // event targets
 let test1_input = document.querySelector('#test1-input')
 let test2_button = document.querySelector('#test2-button')
 let test3_button = document.querySelector('#test3-button')
+let test5_input = document.querySelector('#test5-input')
 
 // outputs
 let test1_output = document.querySelector('#test1-output')
 let test2_output = document.querySelector('#test2-output')
 let test4_output = document.querySelector('#test4-output')
+let test5_output = document.querySelector('#test5-output')
 
 function test1() {
     test1_value = test1_input.value
@@ -35,4 +38,9 @@ function test4() {
             document.getElementById("result").innerHTML
                 = "Favorite Language: " + ele[i].value;
     }
+}
+
+function test5() {
+    test5_list.push(test5_input.value)
+    document.getElementById("list-contents").innerHTML = test5_list
 }
